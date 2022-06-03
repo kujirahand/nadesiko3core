8114fb0f590eba443428e9be40382bf80fe0f9cf
kujirahand <web@kujirahand.com>
2022-06-03 22:26:09 +0900

    v3.3.52

--- src/nako_core_version.mjs
+++ src/nako_core_version.mjs
@@ -1,8 +1,8 @@
 // 実際のバージョン定義 (自動生成されるので以下を編集しない)
 const coreVersion = {
-    version: '3.3.50',
+    version: '3.3.52',
     major: 3,
     minor: 3,
-    patch: 50
+    patch: 52
 };
 export default coreVersion;
